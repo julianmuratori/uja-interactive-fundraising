b5d5151e7d7172e512c71e6e413d6a9e5a2419a4
Julian Muratori <julianmuratori@gmail.com>
2018-07-17 09:24:14 -0400

    backing up the touchscreen app

---
+++ README.md
@@ -0,0 +1,1 @@
+"# uja-interactive-fundraising" 
---
+++ script.js
@@ -0,0 +1,50 @@
+// var $grid = $('.grid').isotope({
+//   itemSelector: '.value-prop'
+// });
+//
+// $grid.imagesLoaded().progress( function() {
+//   $grid.isotope('layout');
+// });
+
+var $grid = $('.grid').imagesLoaded( function() {
+  // init Isotope after all images have loaded
+  $grid.isotope({
+    itemSelector: '.value-prop'
+    // options...
+  });
+});
+
+// store filter for each group
+var filters = {};
+
+$('.filters').on( 'click', '.button', function() {
+  var $this = $(this);
+  // get group key
+  var $buttonGroup = $this.parents('.button-group');
+  var filterGroup = $buttonGroup.attr('data-filter-group');
+  // set filter for group
+  filters[ filterGroup ] = $this.attr('data-filter');
+  // combine filters
+  var filterValue = concatValues( filters );
+  // set filter for Isotope
+  $grid.isotope({ filter: filterValue });
+  console.log(filterValue);
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
